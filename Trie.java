4db286361378fc2027d2da53c6d25734188ceac3
aliuzzi <43051687+aliuzzi@users.noreply.github.com>
2019-11-18 00:34:53 -0800

    Trie.java

--- Trie.java
+++ Trie.java
@@ -1,66 +1,86 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
-public class Trie implements Addable {
+public class Tree implements Addable{
 
-    TrieNode root;
+    private Node<String> root;
 
-    public Trie() {
-        this.root = new TrieNode();
+    public Tree() {
+        root = null;
     }
 
+    public boolean add(String item) {
+        root=add(item, root);
+        return true;
+    }
 
-
-//    public void setRoot(){
-//
-//    }
-//
-//
-//    public void getChildren(){
-//
-//    }
-
-
-
-    @Override
-    public void add(String word) {
-
-        TrieNode current = root;
-        for (int i = 0; i < word.length(); i++) {
-
-            if(current.trieNodes[word.charAt(i)-'a'] ==null){
-                current.trieNodes[word.charAt(i)-'a'] = new TrieNode();
-            }
-            current = current.next(word.charAt(i));
+    public Node<String> add(String item, Node<String> node) {
+        if (node == null) {
+            return new Node<String>(item);
+        } else if (node.data.compareTo(item) < 0) {
+            node.right = add(item, node.right);
+        } else {
+            node.left = add(item, node.left);
         }
-        //current.setEndOfWord(true);
-        current.endOfWord++;
-//        TrieNode current = root;
-//
-//        for (int i = 0; i < word.length(); i++) {
-//            current = current.getChildren()
-//                    .computeIfAbsent(word.charAt(i), c -> new TrieNode());
-//        }
-//        current.setEndOfWord(true);
-
+        return node;
     }
 
-    public static void main(String[] args){
-        final List<String> setOfStrings = new ArrayList<>();
-        setOfStrings.add("pqrs");
-        setOfStrings.add("pprt");
-        setOfStrings.add("psst");
-        setOfStrings.add("qqrs");
-        setOfStrings.add("pqrs");
-        final TrieNode trie = new TrieNode();
-        setOfStrings.forEach(trie::insert);
-        System.out.println(trie.lookUp("psst"));
-        System.out.println(trie.lookUp("psstq"));
-        System.out.println(trie.lookUp("qqrs"));
-        System.out.println(trie.lookUp("psst"));
-
-
-
+    public void print() {
+        print(root);
     }
+
+    public void print(Node<String> node) {
+        if (node != null) {
+            print(node.left);
+            System.out.println(node.data);
+            print(node.right);
+        }
+    }
+
+    public String spellcheck(String item){
+        return spellcheck(item, root);
+    }
+
+    public String spellcheck(String item, Node<String> node) {
+
+        if (node.data.compareTo(item) == 0) {
+            throw new IllegalStateException("exact match found");
+        } else if (node.data.compareTo(item) < 0) {
+            if(node.right==null) {
+                return node.data;
+            } else {
+                return spellcheck(item, node.right);
+            }
+
+        } else {
+            if(node.left==null) {
+                return node.data;
+            } else {
+                return spellcheck(item, node.left);
+            }
+
+        }
+    }
+    public boolean lookUp(String item) {
+
+        return lookUp(item, root);
+    }
+
+    public boolean lookUp(String item, Node<String> node) {
+        if (node == null) {
+            return false;
+        }
+        if (node.data.compareTo(item) == 0) {
+            return true;
+        } else if (node.data.compareTo(item) < 0) {
+            return lookUp(item, node.right);
+        } else {
+            return lookUp(item, node.left);
+        }
+    }
+
 }
+
+
+
+
